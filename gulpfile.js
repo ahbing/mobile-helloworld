72d8d9bb732ba32f57c80f3638e13bd8ec25f67f
念树 <givemearainbowlife@gmail.com>
2015-11-10 10:39:29 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-ruby-sass');
+var imagemin = require('gulp-imagemin');
+gulp.task('js', function () {
+    return gulp.src('src/js/*js')
+        // .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('css', function () {
+    gulp.src('dist/css/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('images', function () {
+    gulp.src('src/images/*.*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('sass', function() {
+    return sass('src/sass/*.scss')
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+   })
+    .pipe(gulp.dest('dist/css'))
+});
+gulp.task('copyvideo', function () {
+    gulp.src('src/video/**/*')
+        .pipe(gulp.dest('dist/video/'))
+});
+gulp.task('copyfonts', function () {
+    gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts/'))
+});
+
+gulp.task('auto',function(){
+  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/js/*js', ['js']);
+  // gulp.watch('dist/css/*.css', ['css']);
+  gulp.watch('src/images/*.*', ['images']);
+  gulp.watch('src/video/*.*', ['copyvideo']);
+  gulp.watch('src/fonts/*.*', ['copyfonts']);
+})
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("dist/js/*.js").on('change', browserSync.reload);
+    gulp.watch("dist/css/*.css").on('change', browserSync.reload);
+    gulp.watch("dist/images/*.*").on('change', browserSync.reload);
+});
+gulp.task('default',['serve','auto'])
---
+++ src/js/main.js
@@ -0,0 +1,82 @@
+
+;(function(){
+	//  呼出 隐藏 侧边栏
+	var wrapper = document.querySelector('.wrapper');
+	var leftNav = document.querySelector('.main-nav');
+	var startX,startTime,moveX,isLeft,showNav;
+	var touchStart = function(e){
+		e.preventDefault();
+		var touch = e.touches[0];
+		startX = touch.pageX;
+	};
+	var touchMove = function(e){
+		e.preventDefault();
+		var touch = e.touches[0];
+		moveX = touch.pageX - startX;
+		// 滑动 距离过短  认为是 点击 直接return掉
+		if(Math.abs(moveX)<200){ return ;}
+		// 是否向左滑动
+		isLeft = moveX > 0 ? false : true;
+	}
+
+	var touchEnd = function(e){
+		e.preventDefault();
+		var isShowNav = leftNav.getAttribute('data-show');
+		var oldClassName,newClassName;
+
+		if(isLeft!== undefined && !isLeft && isShowNav){
+			// 不是左划 且 nav 不显示  呼出
+			oldClassName = 'left-out';
+			newClassName = 'left-in';
+			leftNav.setAttribute('data-show','true');
+		}else if(isLeft && isShowNav){
+		 //  左划 且 nav 已经显示  就隐藏
+			// 隐藏
+			oldClassName = 'left-in';
+			newClassName = 'left-out';
+			leftNav.setAttribute('data-show','fasle');
+		}
+		if(e.target.className == 'more'){
+			oldClassName = 'left-out';
+			newClassName = 'left-in';
+			leftNav.setAttribute('data-show','true');
+		}
+		replaceClass(leftNav,oldClassName,newClassName);
+	}
+	// wrapper 上增加监听
+	wrapper.addEventListener('touchstart',touchStart,false);
+	wrapper.addEventListener('touchmove',touchMove,false);
+	wrapper.addEventListener('touchend',touchEnd,false);
+})();
+
+
+;(function(){
+	var videoPage = document.querySelector('.video-page');
+	if(videoPage){
+		var videoBtn = document.querySelector('.see-video');
+		var closeBtn = document.querySelector('.close-video-btn');
+		var video = videoPage.querySelector('video');
+		var hasViode = !!(document.createElement('video').canPlayType)
+		if(!hasViode) return;
+		var src = video.getAttribute('data-src');
+		var showVideo = function(e){
+			// 检测浏览器是否支持video
+			e.preventDefault();
+			//  显示播放页面
+			video.style.display = 'block';
+			video.setAttribute('src',src);
+			console.log("show");
+			replaceClass(videoPage,'hide-video-page','show-video-page');
+
+		}
+		var hideVideo = function(e){
+			e.preventDefault();
+			console.log("hide");
+			replaceClass(videoPage,'show-video-page','hide-video-page');
+			video.setAttribute('src','');
+			video.style.display = 'none';
+		}
+		videoBtn.addEventListener('touchend',showVideo,false);
+		closeBtn.addEventListener('touchend',hideVideo,false);
+	}
+})();
